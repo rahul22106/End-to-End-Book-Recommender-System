c91bdb437d0177e71da9a3b8dee2203eb6d975b3
rahul22106 <rahulkrmishra2004@gmail.com>
2025-11-01 08:27:21 +0530

    Data ingestion added

--- books_recommender/utils/util.py
+++ books_recommender/utils/util.py
@@ -13,4 +13,4 @@ def read_yaml_file(file_path:str)->dict:
         with open(file_path, 'rb') as yaml_file:
             return yaml.safe_load(yaml_file)
     except Exception as e:
-        raise AppException(e,sys) from e
+        raise AppException(e,sys) from e 
